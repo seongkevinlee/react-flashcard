199d924d32ea5974599d5af07455ab2f826c5d0a
seongkevinlee <seongkevinlee@gmail.com>
2020-06-09 15:56:25 -0700

    Added ability to switch between questions on review component

--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -5,44 +5,67 @@ export default class ReviewCards extends React.Component {
     super(props);
     this.state = {
       activeCardIndex: 0
-      // cardFront: this.props.cards[this.state.activeCardIndex].question,
-      // cardBack: this.props.cards[this.state.activeCardIndex].answer
     };
 
     this.nextCard = this.nextCard.bind(this);
-    this.setActiveCard = this.setActiveCard.bind(this);
+    this.previousCard = this.previousCard.bind(this);
+    // this.setActiveCard = this.setActiveCard.bind(this);
   }
 
-  componentDidMount() {
-    this.setActiveCard();
-    // this.nextCard();
-  }
+  // componentDidMount() {
+  //   this.setActiveCard();
+  // }
 
-  setActiveCard() {
-    const cardList = this.props.cards;
-    const activeCard = cardList[this.state.activeCardIndex];
-    this.setState({
-      activeCard
-    });
-  }
+  // setActiveCard() {
+
+  // }
 
   nextCard() {
+    let nextCardIndex = this.state.activeCardIndex;
+    nextCardIndex++;
     this.setState({
-      cardFront: this.props.activeCard.question,
-      cardBack: this.props.activeCard.answer
+      activeCardIndex: nextCardIndex
     });
-    // const { cards } = this.props;
-    // const { activeCard } = this.props;
-    // console.log('nextCard props:', this.props);
-    // const nextCardIndex = activeCard.indexOf(activeCard);
-    // console.log('nextCardIndex:', nextCardIndex);
+  }
+
+  previousCard() {
+    let nextCardIndex = this.state.activeCardIndex;
+    nextCardIndex--;
+    this.setState({
+      activeCardIndex: nextCardIndex
+    });
   }
 
   render() {
+    const cardList = this.props.cards;
+    const cardFront = cardList[this.state.activeCardIndex].question;
+    // const cardBack = cardList[this.state.activeCardIndex].answer;
+
     return (
       <div>
         <h1 className='text-center' onClick={this.nextCard}>Review Cards</h1>
-
+        <div id='carouselControls' className="carousel slide carousel-container d-flex align-items-center" data-ride='carousel'>
+          <div className="carousel-inner">
+            <div className="carousel-item active d-flex justify-content-center align-items-center">
+              <h4>{cardFront}</h4>
+            </div>
+          </div>
+          <a id='carousel-prev'
+            className="carousel-control-prev"
+            role='button'
+            data-slide='prev'>
+            <span className="carousel-control-prev-icon"
+              onClick={this.previousCard}></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            role='button'
+            data-slide='next'>
+            <span className="carousel-control-next-icon" onClick={this.nextCard}></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
       </div>
     );
   }
